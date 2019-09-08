eb2c940a70a99bab89a10e15d6af50c2c387a0fe
meeting_film@sina.com <meeting_film@sina.com>
2019-09-08 16:20:28 +0800

    添加跳转链接

--- src/views/notice.vue
+++ src/views/notice.vue
@@ -46,7 +46,6 @@ export default {
   },
   async mounted () {
     await this.setMenu()
-<<<<<<< Updated upstream
     this.noticeNav.forEach(async (item, index) => {
       if (item.type !== 'perchase') {
         this.queryOption[index] = Object.assign({}, JSON.parse(JSON.stringify(dataset.queryOption)), { cat_id: item.id })
@@ -66,11 +65,6 @@ export default {
         this.perchaseList = (await this.getTicketsList(this.queryOption1)).data
       }
     })
-=======
-
-    this.queryOption = Object.assign({}, { start: 0, limit: 10 })
-    this.perchaseList = (await this.getTicketsList(this.queryOption)).data
->>>>>>> Stashed changes
   },
   // mounted() {
   //   // getTicketsList
@@ -152,184 +146,185 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.m-notice-page {
-  .banner-wrapper {
-    img {
-      max-width 100%
-    }
-  }
-  >>> .m-com-tab {
-    margin: 25px 0
-    height 50px
-    .m-com-nav {
-      padding 15px 26px
-      background-color #ffffff
-      border-radius 25px
-      font-size 20px
-      color #000000
-      text-align center
-      transition all .2s ease
-      &.active, &:hover {
-        background-color #ec5a02
-        color #ffffff
+  .m-notice-page {
+    .banner-wrapper {
+      img {
+        max-width 100%
       }
     }
-    .m-com-nav + .m-com-nav {
-      margin-left 20px
+    >>> .m-com-tab {
+      margin: 25px 0
+      height 50px
+      .m-com-nav {
+        padding 15px 26px
+        background-color #ffffff
+        border-radius 25px
+        font-size 20px
+        color #000000
+        text-align center
+        transition all .2s ease
+        &.active, &:hover {
+          background-color #ec5a02
+          color #ffffff
+        }
+      }
+      .m-com-nav + .m-com-nav {
+        margin-left 20px
+      }
     }
-  }
-  >>> .m-com-transition {
-    .notice-wrapper, .price-wrapper, .perchase-wrapper {
-      width 1200px
-      margin 0 auto
-      min-height 400px
-    }
-    .notice-wrapper {
-      ul {
-        li {
-          position: relative
-          border-top:1px dashed #999
-          padding:25px 0
-          overflow: hidden
-          &:first-child {
-            border-top:none
-          }
-          &:before {
-            content attr(data-before)
-            color: #ec5a02
-          }
-          &:after {
-            content attr(data-after)
-            color: #999
-            float: right
-          }
-          .txt-title {
-            display: inline-block
-            color: #333
+    >>> .m-com-transition {
+      .notice-wrapper, .price-wrapper, .perchase-wrapper {
+        width 1200px
+        margin 0 auto
+        min-height 400px
+      }
+      .notice-wrapper {
+        ul {
+          li {
+            position: relative
+            border-top:1px dashed #999
+            padding:25px 0
+            overflow: hidden
+            &:first-child {
+              border-top:none
+            }
+            &:before {
+              content attr(data-before)
+              color: #ec5a02
+            }
+            &:after {
+              content attr(data-after)
+              color: #999
+              float: right
+            }
+            .txt-title {
+              display: inline-block
+              color: #333
+            }
           }
         }
       }
-    }
-    .price-wrapper {
-      .txt-wrapper {
-        margin-bottom:40px
-        &:before {
-          content attr(data-before)
-          display: block
-          font-size 18px
-          color: #ec5a02
-        }
-        .txt {
-          font-size 14px
-          color: #333
-          line-height:30px
-        }
-      }
-    }
-    .perchase-wrapper {
-      ul {
-        li {
-          display: flex
-          margin-bottom:50px
-          position: relative
-          padding-top:50px
+      .price-wrapper {
+        .txt-wrapper {
+          margin-bottom:40px
           &:before {
             content attr(data-before)
             display: block
-            position: absolute
-            color: #ec5a02
             font-size 18px
-            top:20px
+            color: #ec5a02
           }
-          .img-wrapper {
-            flex 0 0 227px
-            padding-right: 30px
-            overflow: hidden
-            img {
-              max-width 100%
+          .txt {
+            font-size 14px
+            color: #333
+            line-height:30px
+          }
+        }
+      }
+      .perchase-wrapper {
+        ul {
+          li {
+            display: flex
+            margin-bottom:50px
+            position: relative
+            padding-top:50px
+            &:before {
+              content attr(data-before)
+              display: block
+              position: absolute
+              color: #ec5a02
+              font-size 18px
+              top:20px
             }
-          }
-          .txt-wrapper {
-            flex 1
-            .title-group {
-              line-height:26px
-              .txt-title {
-                font-size 18px
-                color: #000
-                margin-right:15px
+            .img-wrapper {
+              flex 0 0 227px
+              padding-right: 30px
+              overflow: hidden
+              img {
+                max-width 100%
               }
-              .txt-subtitle {
+            }
+            .txt-wrapper {
+              flex 1
+              .title-group {
+                line-height:26px
+                .txt-title {
+                  font-size 18px
+                  color: #000
+                  margin-right:15px
+                }
+                .txt-subtitle {
+                  font-size 14px
+                  color: #666
+                }
+              }
+              .txt-summary {
                 font-size 14px
                 color: #666
+                line-height:26px
+              }
+              .txt-address, .txt-time {
+                font-size 14px
+                color: #000
+                line-height:26px
+                &:before {
+                  content attr(data-before)
+                }
+              }
+              .btn-group {
+                .btn-enter, .btn-safe {
+                  display: inline-block
+                  vertical-align: middle
+                  font-size 14px
+                  padding:7px 10px
+                  border-radius 4px
+                  margin-right:10px
+                }
+                .btn-enter {
+                  color: #5cb85c
+                  border 1px solid #5cb85c
+                  &:hover {
+                    background-color: #5cb85c
+                    color: #fff
+                  }
+                }
+                .btn-safe {
+                  color: #5bc0de
+                  border 1px solid #5bc0de
+                  &:hover {
+                    background-color: #5bc0de
+                    color: #fff
+                  }
+                }
               }
             }
-            .txt-summary {
-              font-size 14px
-              color: #666
-              line-height:26px
-            }
-            .txt-address, .txt-time {
-              font-size 14px
-              color: #000
-              line-height:26px
-              &:before {
-                content attr(data-before)
+            .buy-wrapper {
+              flex 104px 0 0
+              img {
+                max-width 100%
+                padding:3px
+                border 1px solid #ec5a02
+                margin-bottom:18px
               }
-            }
-            .btn-group {
-              .btn-enter, .btn-safe {
+              .btn-buy {
+                border 1px solid #ec5a02
+                background-color: #ec5a02
+                color: #fff
+                font-size 18px
                 display: inline-block
                 vertical-align: middle
-                font-size 14px
-                padding:7px 10px
+                padding:6px 20px
+                width: 73px
+                text-align: center
                 border-radius 4px
-                margin-right:10px
-              }
-              .btn-enter {
-                color: #5cb85c
-                border 1px solid #5cb85c
+                -webkit-transition: all .2 ease .1s
+                -moz-transition: all .2 ease .1s
+                -ms-transition: all .2 ease .1s
+                -o-transition: all .2 ease .1s
+                transition: all .2 ease .1s
                 &:hover {
-                  background-color: #5cb85c
-                  color: #fff
+                  background-color: #fff
+                  color: #ec5a02
                 }
-              }
-              .btn-safe {
-                color: #5bc0de
-                border 1px solid #5bc0de
-                &:hover {
-                  background-color: #5bc0de
-                  color: #fff
-                }
-              }
-            }
-          }
-          .buy-wrapper {
-            flex 104px 0 0
-            img {
-              max-width 100%
-              padding:3px
-              border 1px solid #ec5a02
-              margin-bottom:18px
-            }
-            .btn-buy {
-              border 1px solid #ec5a02
-              background-color: #ec5a02
-              color: #fff
-              font-size 18px
-              display: inline-block
-              vertical-align: middle
-              padding:6px 20px
-              width: 73px
-              text-align: center
-              border-radius 4px
-              -webkit-transition: all .2 ease .1s
-              -moz-transition: all .2 ease .1s
-              -ms-transition: all .2 ease .1s
-              -o-transition: all .2 ease .1s
-              transition: all .2 ease .1s
-              &:hover {
-                background-color: #fff
-                color: #ec5a02
               }
             }
           }
@@ -337,5 +332,4 @@ export default {
       }
     }
   }
-}
 </style>
